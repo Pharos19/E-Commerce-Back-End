d33859a6e9f5cb0a269a9d0c8ee6fe78463609f4
Pharos19 <Pharos199@gmail.com>
2023-02-21 19:55:00 -0700

    fixed errors

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-/ get one product
+// get one product
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
