1cb3019dd25701af47d99ddeaba6bf301fb3a205
Pharos19 <Pharos199@gmail.com>
2023-02-21 21:04:26 -0700

    connect to jawsdb

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
